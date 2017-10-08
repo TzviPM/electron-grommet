8aa5d8139848938201fd2215a46f6f3f4fa259ca
Tzvi Melamed <hartley@melamed.biz>
2017-10-08 02:39:21 -0400

    Initial ewqs for Tzvi

---
+++ src/renderer/main.tsx
@@ -0,0 +1,33 @@
+//import styles
+import '../../config/scss/vanilla/index.scss';
+
+import * as React from 'react';
+import App from 'grommet/components/App';
+import Box from 'grommet/components/Box';
+import Header from 'grommet/components/Header';
+import Footer from 'grommet/components/Footer';
+import Meter from 'grommet/components/Meter';
+import Title from 'grommet/components/Title';
+// import Value from 'grommet/components/Value';
+
+export default class extends React.Component {
+  render() {
+    return (
+      <App centered={false}>
+        <Header direction="row" justify="between" size="large"
+          pad={{ horizontal: 'medium' }}>
+          <Title>Grommet standalone</Title>
+        </Header>
+        <Box pad='medium'>
+          <Meter value={40} />
+        </Box>
+        <Footer primary={true} appCentered={true} direction="column"
+          align="center" pad="small" colorIndex="grey-1">
+          <p>
+            Build your ideas with <a href="http://grommet.io" target="_blank">Grommet</a>!
+          </p>
+        </Footer>
+      </App>
+    );
+  }
+};
